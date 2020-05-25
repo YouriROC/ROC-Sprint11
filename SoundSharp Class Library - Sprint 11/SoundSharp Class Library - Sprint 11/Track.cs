eeeca416ab28d96578066f8896b58958bb1d629b
Youri <0320494@student.rocvantwente.nl>
2020-05-25 10:41:16 +0200

    clear

--- SoundSharp Class Library - Sprint 11/SoundSharp Class Library - Sprint 11/Track.cs
+++ SoundSharp Class Library - Sprint 11/SoundSharp Class Library - Sprint 11/Track.cs
@@ -6,49 +6,5 @@ using System.Threading.Tasks;
 
 namespace SoundSharp_Class_Library___Sprint_11
 {
-    public enum Category
-    {
-        Ambient,
-        Blues,
-        Country,
-        Disco,
-        Electro,
-        Hardcore,
-        HardRock,
-        HeavyMetal,
-        Hiphop,
-        Jazz,
-        Jumpstyle,
-        Klassiek,
-        Latin,
-        Other,
-        Pop,
-        Punk,
-        Reggae,
-        Rock,
-        Soul,
-        Trance,
-        Techno
-    }
 
-    public struct Time
-    {
-        public int seconds { get; set; }
-        public int minutes { get; set; }
-        public int hours { get; set; }
-    }
-
-
-
-    public class Track
-    {
-        private int id;
-        private string name;
-        private string artist;
-        private string albumSource;
-        
-    }
-
-
-    Time a = new Time(100);
 }
